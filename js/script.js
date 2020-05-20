ca7719c95182b91d90ff5c3313af1e08a2ab5ed3
Emil Bruun Larsen <60384288+Emilovich98@users.noreply.github.com>
2020-05-20 10:56:23 +0200

    Færdig menu
    Har kan der indsættes undersider

--- js/script.js
+++ js/script.js
@@ -36,19 +36,25 @@ function showDesktopNav() {
     let showDesktopMenu = document.querySelector("#desktop-menu-overlay");
     let desktopBurgerLine1 = document.querySelector("#desktop-burger-line-1");
     let desktopBurgerLine3 = document.querySelector("#desktop-burger-line-3");
-    let showWhiteIcons = document.querySelector(".white-icons");
-    let showBlackIcons = document.querySelector(".black-icons");
+    let showWhiteIcons = document.querySelector("#social-media-icons-white");
+    let showBlackIcons = document.querySelector("#social-media-icons-black");
+    let showArrow = document.querySelector("#arrow-down");
     
-    if (showDesktopMenu.style.width === "50vw") {
-        showDesktopMenu.style.width = "0vw";
+    if (showDesktopMenu.style.width === "768px") {
+        showDesktopMenu.style.width = "0px";
         desktopBurgerLine1.style.backgroundColor = "white";
         desktopBurgerLine3.style.backgroundColor = "white";
+        showWhiteIcons.style.display = "block";
+        showBlackIcons.style.display = "none";
+        showArrow.style.display = "flex";
         
     } else {
-        showDesktopMenu.style.width = "50vw";
+        showDesktopMenu.style.width = "768px";
         desktopBurgerLine1.style.backgroundColor = "black";
         desktopBurgerLine3.style.backgroundColor = "black";
-        
+        showWhiteIcons.style.display = "none";
+        showBlackIcons.style.display = "block";
+        showArrow.style.display = "none";
     }
     
      // burger animation
