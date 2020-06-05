25114dd1ee35861369eee72958d05a9f1396a1a0
Marcusfay <60384283+holmtottrup@gmail.com>
2020-06-05 12:54:32 +0200

    Javascript
    Opacity transistiion on scroll

--- js/script.js
+++ js/script.js
@@ -57,6 +57,20 @@ function showDesktopNav() {
 }
 
 
+const topLogo = document.querySelector("#top-logo");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction(){
+    if (window.pageYOffset > 300){
+        topLogo.style.opacity = "100";
+    }
+    else{
+        topLogo.style.opacity = "0";
+    }
+}
+
+
 
 
 
